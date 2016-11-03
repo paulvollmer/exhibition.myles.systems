169e068197db2faced44f616468db5ad8472f76a
el-j <bluearc.in.sky@gmail.com>
2016-11-03 12:01:49 +0100

    add first waypoints

--- js/myles.js
+++ js/myles.js
@@ -1,1 +1,27 @@
 console.log('myles.js');
+
+var waypoint0 = new Waypoint({
+  element: document.getElementById('wp0'),
+  handler: function() {
+  var tmp = document.getElementById('hi');
+  if (tmp.className.match(/(?:^|\s)hi(?!\S)/)){
+  // tmp = tmp.className.replace(/(?:^|\s)leftHeadline(?!\S)/);
+  console.log('remove hidden');
+  tmp = tmp.className += ' show';
+  }
+    console.log('waypoint whas triggered');
+  }
+});
+
+var waypoint1 = new Waypoint({
+  element: document.getElementById('wp1'),
+  handler: function() {
+  var tmp = document.getElementById('whatHead');
+  if (tmp.className.match(/(?:^|\s)leftHeadline(?!\S)/)){
+  // tmp = tmp.className.replace(/(?:^|\s)leftHeadline(?!\S)/);
+  console.log('remove hidden');
+  tmp = tmp.className += ' show';
+  }
+    console.log('waypoint whas triggered');
+  }
+});
